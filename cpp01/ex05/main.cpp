894476f25f2001192a7cf47ff6831d4c99f9b70b
Gropopus <thomas_sembely@gmail.com>
2021-08-16 19:18:56 +0200

    --->

--- cpp01/ex05/main.cpp
+++ cpp01/ex05/main.cpp
@@ -6,12 +6,14 @@
 /*   By: thsembel <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/16 18:48:58 by thsembel          #+#    #+#             */
-/*   Updated: 2021/08/16 18:50:05 by thsembel         ###   ########.fr       */
+/*   Updated: 2021/08/16 19:18:44 by thsembel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "Karen.hpp"
 
+
+//token :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
 int		main(void)
 {
 	Karen karen;
